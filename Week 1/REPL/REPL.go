19aba291c454d9ceef03af6807834daefb5d8d7b
Triet Truong <truongminhtriet96@gmail.com>
2019-07-20 01:08:52 +0700

    Clean code

--- Week 1/REPL/REPL.go
+++ Week 1/REPL/REPL.go
@@ -9,44 +9,55 @@ import (
 	"strings"
 )
 
-//eval
-func eval(input string) (result string, err error) {
+func validate(input string) (a, b float64, op string, err error) {
 	s := strings.Split(strings.Trim(input, "\n"), " ")
-	if len(s) < 2 {
-		return "", errors.New("Invalid format!")
+	err = errors.New("Invalid input")
+	if len(s) != 3 {
+		return 0, 0, "", err
 	}
 	num1, err1 := strconv.ParseFloat(s[0], 64)
 	num2, err2 := strconv.ParseFloat(s[2], 64)
-	op := s[1]
-
-	if err1 == nil && err2 == nil {
-		result := 0.0
-		switch op {
-		case "+":
-			result = num1 + num2
-		case "-":
-			result = num1 - num2
-		case "*":
-			result = num1 * num2
-		case "/":
-			if num2 == 0 {
-				return "", errors.New("Divided by zero!")
-			}
-			result = num1 / num2
-		default:
-			return "", errors.New("Unrecognized operator!")
-		}
-		return fmt.Sprint("> ", num1, " ", op, " ", num2, " = ", result), nil
-	} else {
-		return "", errors.New("ERROR!")
+	op = s[1]
+	if err1 != nil || err2 != nil || strings.ContainsAny(op, "+-/*") == false {
+		return 0, 0, "", err
 	}
+	return num1, num2, op, nil
+}
 
+//eval
+func eval(input string) (result string, err error) {
+	num1, num2, operator, err := validate(input)
+
+	if err != nil {
+		return "", err
+	}
+	var res float64
+	switch operator {
+	case "+":
+		res = num1 + num2
+	case "-":
+		res = num1 - num2
+	case "*":
+		res = num1 * num2
+	case "/":
+		if num2 == 0 {
+			return "", errors.New("divided by zero")
+		}
+		res = num1 / num2
+	default:
+		return "", errors.New("unrecognized operator")
+	}
+	return fmt.Sprint("> ", num1, " ", operator, " ", num2, " = ", res), nil
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		input := scanner.Text()
+		if strings.ToLower(input) == "exit" {
+			break
+		}
+
 		result, err := eval(input)
 		if err == nil {
 			fmt.Println(result)
